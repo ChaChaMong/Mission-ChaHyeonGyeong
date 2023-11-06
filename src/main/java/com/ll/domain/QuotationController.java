ae66fb08e304224659a5d601a0cfadfa9460a5c3
unknown <thflf93@gmail.com>
2023-11-06 10:34:06 +0900

    44단계

--- src/main/java/com/ll/domain/QuotationController.java
+++ src/main/java/com/ll/domain/QuotationController.java
@@ -7,19 +7,24 @@ import java.util.Scanner;
 public class QuotationController {
     private Scanner scanner;
     private List<Quotation> quotations;
+    private int lastQuotationId;
 
     public QuotationController(Scanner scanner) {
         this.scanner = scanner;
         quotations = new ArrayList<>();
+        lastQuotationId = 0;
     }
     public void actionWrite() {
         System.out.print("명언 : ");
         String content = scanner.nextLine();
 
-        System.out.print("명언 : ");
+        System.out.print("작가 : ");
         String authorName = scanner.nextLine();
 
-        Quotation quotation = new Quotation(quotations.size() + 1, content, authorName);
+        lastQuotationId++;
+        int id = lastQuotationId;
+
+        Quotation quotation = new Quotation(id, content, authorName);
         quotations.add(quotation);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", quotation.getId());
